4baea3b1bf8932abe78ebeb822fa69c324ae5a31
Ntare Guy <gntare2@gmail.com>
2024-11-17 20:31:33 +0200

    auth init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Alumnus Network API')
+    .setDescription('connect - engage - empower')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  app.enableCors();
+
+  await app.listen(process.env.PORT);
+  console.log(`🚀 Server ready at: http://localhost:${process.env.PORT}`);
 }
 bootstrap();
