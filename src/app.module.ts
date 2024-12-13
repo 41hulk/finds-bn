a0117d38764195dd82610b36720d21652c826a83
Ntare Guy <gntare2@gmail.com>
2024-12-13 20:42:58 +0200

    fix typo

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,9 +12,16 @@ import { ConfigModule } from '@nestjs/config';
 import { BookingModule } from './booking/booking.module';
 import { WaitlistModule } from './waitlist/waitlist.module';
 import { RfpModule } from './rfp/rfp.module';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './auth/jwt.strategy';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '15m' },
+    }),
     AuthModule,
     UsersModule,
     ConfigModule.forRoot({ isGlobal: true }),
@@ -32,6 +39,11 @@ import { RfpModule } from './rfp/rfp.module';
     RfpModule,
   ],
   controllers: [AppController],
-  providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }, AppService],
+  providers: [
+    { provide: APP_GUARD, useClass: ThrottlerGuard },
+    AppService,
+    JwtStrategy,
+    PrismaService,
+  ],
 })
 export class AppModule {}
